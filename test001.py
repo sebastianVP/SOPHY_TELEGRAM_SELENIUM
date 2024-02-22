396087caa9204d3a595a203a46efecbc4cdd0044
Alexander Valdez <alex.valdezp22@gmail.com>
2024-02-22 11:02:14 -0500

    Primera prueba

---
+++ test001.py
@@ -0,0 +1,8 @@
+#https://chromedriver.chromium.org/downloads
+#https://telepot.readthedocs.io/en/latest/
+#https://selenium-python.readthedocs.io/locating-elements.html
+#https://stackoverflow.com/questions/27948420/click-at-at-an-arbitrary-position-in-web-browser-with-selenium-2-python-binding
+
+from selenium import webdriver
+driver    = webdriver.Chrome(executable_path="/home/soporte/Downloads/chromedriver_linux64/chromedriver",options=driver_options)
+
